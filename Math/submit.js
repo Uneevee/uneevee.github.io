58ffaf290124eb3d962c7cae4e81481deec6b8da
Uneevee <georgiabulldog419@gmail.com>
2023-11-29 09:35:38 -0600

    nvm i think I finally got it

--- Math/submit.js
+++ Math/submit.js
@@ -1,1 +1,9 @@
-document.getElementById("response").innerHTML = document.getElementById("Answer").innerHTML
+var input = document.createElement("input")
+input.setAttribute("id", "response")
+input.setAttribute("type", "text")
+document.body.insertBefore(input, null)
+console.log(input)
+
+function submit(){
+    document.getElementById("response").innerHTML = input
+}
