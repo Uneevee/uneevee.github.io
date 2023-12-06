d321634e8b5913c03044e203cb7e1b0f3d9fdbdb
Uneevee <georgiabulldog419@gmail.com>
2023-12-06 13:08:37 -0600

    realized i did a bit much

--- adventofcodechallenges/Day1/challenge.js
+++ adventofcodechallenges/Day1/challenge.js
@@ -1,10 +1,8 @@
-var result = document.createElement("h3")
-result.setAttribute("id", "result")
-document.body.insertBefore(input, null)
-newquestion()
+var result = document.createElement("h3");
+result.setAttribute("id", "result");
+document.body.insertBefore(input, null);
 
 function getanswer(){
-    var page = new XMLHttpRequest()
-    page.open("GET", "https://adventofcode.com/2023/day/1/input")
-    document.getElementById("result").innerHTML = page.responseText
+    all = fetch("challenge.json")
+    
 }
