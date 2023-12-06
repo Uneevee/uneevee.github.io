3a226fccedc1a22de244be96016126f6afadd3ed
Uneevee <georgiabulldog419@gmail.com>
2023-12-06 10:36:01 -0600

    gets page for day 1

--- adventofcodechallenges/Day1/challenge.js
+++ adventofcodechallenges/Day1/challenge.js
@@ -1,9 +1,4 @@
 var result = document.createElement("h3")
-var num1 = Math.floor(Math.random()*10)
-var num2 = Math.floor(Math.random()*10)
-var answer
-var mode = 0
-var modes = [addition]
 result.setAttribute("id", "result")
 document.body.insertBefore(input, null)
 newquestion()
