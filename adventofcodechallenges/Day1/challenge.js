47abb573ef0b263c7dc067e7ff87c8b67ad90a7b
Uneevee <georgiabulldog419@gmail.com>
2024-01-26 09:37:57 -0600

    great undoing of advent of code

--- adventofcodechallenges/Day1/challenge.js
+++
@@ -1,8 +0,0 @@
-var result = document.createElement("h3");
-result.setAttribute("id", "result");
-document.body.insertBefore(input, null);
-
-function getanswer(){
-    all = fetch("challenge.json")
-    
-}
