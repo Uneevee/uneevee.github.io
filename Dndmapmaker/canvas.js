31de5a47d3c7586f2edc1fd398015eb593fac3d7
Uneevee <Georgiabulldog419@gmail.com>
2024-04-10 17:17:24 -0500

    everything is better now :>

--- Dndmapmaker/canvas.js
+++ Dndmapmaker/canvas.js
@@ -5,5 +5,8 @@ var mousemove = (evnt)=>{subject.style.left = evnt.pageX +"px";
     subject.style.position = "absolute";
     subject.style.border = "0px";
 }
+function newelement(name){
+    subject = document.getElementById(name);
+}
 document.addEventListener("mousemove", mousemove);
 document.addEventListener("mouseup", (evnt)=>{subject=document.getElementById("placehold");});
