c88b775ec58c020b5daf82e374fcf7950cdb5850
Uneevee <Georgiabulldog419@gmail.com>
2024-04-10 00:38:30 -0500

    WORK NOW

--- Dndmapmaker/canvas.js
+++ Dndmapmaker/canvas.js
@@ -1,4 +1,8 @@
 var subject = document.getElementById("placehold");
-var mousemove = (evnt)=>{subject.style.left = evnt.pageX +"px" ; subject.style.top = evnt.pageY+ "px";}
+var mousemove = (evnt)=>{subject.style.left = evnt.pageX +"px";
+    subject.style.top = evnt.pageY + "px";
+    subject.style.transform = "0deg";
+}
 document.addEventListener("mousemove", mousemove);
 document.addEventListener("mouseup", (evnt)=>{subject=document.getElementById("placehold");});
+subject.setAttribute()
