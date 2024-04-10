3b9db3783c8f00a6d5ad57056ac508493aae0db1
Uneevee <Georgiabulldog419@gmail.com>
2024-04-10 00:43:34 -0500

    maybe adding position and everything will help?

--- Dndmapmaker/canvas.js
+++ Dndmapmaker/canvas.js
@@ -2,7 +2,8 @@ var subject = document.getElementById("placehold");
 var mousemove = (evnt)=>{subject.style.left = evnt.pageX +"px";
     subject.style.top = evnt.pageY + "px";
     subject.style.transform = "0deg";
+    subject.style.position = "absolute";
+    subject.style.border = "0px";
 }
 document.addEventListener("mousemove", mousemove);
 document.addEventListener("mouseup", (evnt)=>{subject=document.getElementById("placehold");});
-subject.setAttribute()
