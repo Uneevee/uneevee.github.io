86feed6069414ed50af50d4750300ade827d7aca
Uneevee <Georgiabulldog419@gmail.com>
2024-04-14 15:41:24 -0500

    no more snapping :>

--- Dndmapmaker/canvas.js
+++ Dndmapmaker/canvas.js
@@ -1,13 +1,22 @@
 var subject = document.getElementById("placehold");
-var mousemove = ()=>{subject.style.left = evnt.pageX +"px";
-    subject.style.top = evnt.pageY + "px";
+subject.style.left = 0
+var pos = [0, 0]
+var mousemove = (evnt)=>{subject.style.left = (parseInt(subject.style.left) + (evnt.pageX - pos[0])) +"px";
+    pos[0] = evnt.pageX
+    subject.style.top = (parseInt(subject.style.top) + (evnt.pageY - pos[1])) +"px";
+    pos[1] = evnt.pageY
     subject.style.rotate = "99deg";
     subject.style.position = "absolute";
-    subject.style.transform
+    //subject.style.transform
 }
 function newelement(name){
     subject = document.getElementById(name);
 }
+var mousedown = (evnt)=>{ 
+    subject.style.left = subject.offsetLeft + "px"
+    subject.style.top = subject.offsetTop + "px"
+}
 document.addEventListener("mousemove", mousemove);
 document.addEventListener("mouseup", ()=>{subject=document.getElementById("placehold");});
-document.addEventListener("wheel", ()=>{});
+document.addEventListener("mousedown", mousedown)
+//document.addEventListener("wheel", ()=>{});
