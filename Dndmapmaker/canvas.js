c387fbd51d2cab00d101bb333918c29f50dfa539
Uneevee <Georgiabulldog419@gmail.com>
2024-04-15 23:35:13 -0500

    copied over changes from other computer wouldn't connect to internet to sync changes

--- Dndmapmaker/canvas.js
+++ Dndmapmaker/canvas.js
@@ -1,6 +1,7 @@
 var subject = document.getElementById("placehold");
 subject.style.left = 0
 var pos = [0, 0]
+
 var mousemove = (evnt)=>{subject.style.left = (parseInt(subject.style.left) + (evnt.pageX - pos[0])) +"px";
     pos[0] = evnt.pageX
     subject.style.top = (parseInt(subject.style.top) + (evnt.pageY - pos[1])) +"px";
@@ -12,10 +13,10 @@ var mousemove = (evnt)=>{subject.style.left = (parseInt(subject.style.left) + (e
 function newelement(name){
     subject = document.getElementById(name);
 }
-var mousedown = (evnt)=>{ 
-    subject.style.left = subject.offsetLeft + "px"
+var mousedown = (evnt)=>{subject.style.left = subject.offsetLeft + "px"
     subject.style.top = subject.offsetTop + "px"
 }
+//TODO: MAKE RESIZE AND ROTATE BOX
 document.addEventListener("mousemove", mousemove);
 document.addEventListener("mouseup", ()=>{subject=document.getElementById("placehold");});
 document.addEventListener("mousedown", mousedown)
