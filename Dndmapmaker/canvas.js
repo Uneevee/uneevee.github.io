a6f7f1ac9ffcd89d240088b8264e9c07047642b9
Uneevee <Georgiabulldog419@gmail.com>
2024-04-15 22:59:38 -0500

    better movement and I had to do weird debugging

--- Dndmapmaker/canvas.js
+++ Dndmapmaker/canvas.js
@@ -1,10 +1,12 @@
 var subject = document.getElementById("placehold");
-subject.style.left = 0
-var pos = [0, 0]
-var mousemove = (evnt)=>{subject.style.left = (parseInt(subject.style.left) + (evnt.pageX - pos[0])) +"px";
-    pos[0] = evnt.pageX
+subject.style.left = 0;
+var pos = [0, 0];
+
+var mousemove = (evnt)=>{
+    subject.style.left = (parseInt(subject.style.left) + (evnt.pageX - pos[0])) +"px";
+    pos[0] = evnt.pageX;
     subject.style.top = (parseInt(subject.style.top) + (evnt.pageY - pos[1])) +"px";
-    pos[1] = evnt.pageY
+    pos[1] = evnt.pageY;
     subject.style.rotate = "99deg";
     subject.style.position = "absolute";
     //subject.style.transform
@@ -12,11 +14,17 @@ var mousemove = (evnt)=>{subject.style.left = (parseInt(subject.style.left) + (e
 function newelement(name){
     subject = document.getElementById(name);
 }
-var mousedown = (evnt)=>{ 
-    subject.style.left = subject.offsetLeft + "px"
-    subject.style.top = subject.offsetTop + "px"
+var mousedown = (evnt)=>{
+    subject.style.left = subject.offsetLeft + "px";
+    subject.style.top = subject.offsetTop + "px";
+
 }
+var mouseup = (evnt)=>{
+    subject=document.getElementById("placehold");
+    resizing = false;
+}
+//TODO: MAKE RESIZE AND ROTATE BOX ITSELF
 document.addEventListener("mousemove", mousemove);
-document.addEventListener("mouseup", ()=>{subject=document.getElementById("placehold");});
-document.addEventListener("mousedown", mousedown)
+document.addEventListener("mouseup", mouseup);
+document.addEventListener("mousedown", mousedown);
 //document.addEventListener("wheel", ()=>{});
