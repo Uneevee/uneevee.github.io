e7007436da12a76366a223e29e19b2de7f3f1f74
Uneevee <Georgiabulldog419@gmail.com>
2024-04-17 01:08:07 -0500

    creation of elements along with adapted sizes

--- Dndmapmaker/canvas.js
+++ Dndmapmaker/canvas.js
@@ -1,27 +1,47 @@
 var subject = document.getElementById("placehold");
-subject.style.left = 0
-var pos = [0, 0]
+subject.style.left = 0;
+var pos = [0, 0];
+var props = [];
+var elementcount = 0;
 
 var mousemove = (evnt)=>{subject.style.left = (parseInt(subject.style.left) + (evnt.pageX - pos[0])) +"px";
-    pos[0] = evnt.pageX
+    pos[0] = evnt.pageX;
     subject.style.top = (parseInt(subject.style.top) + (evnt.pageY - pos[1])) +"px";
     pos[1] = evnt.pageY;
-    subject.style.rotate = "99deg";
+    subject.style.rotate = "0deg";
     subject.style.position = "absolute";
     //subject.style.transform
 }
 function newelement(name){
     subject = document.getElementById(name);
+    subject.style.min
 }
-function spawnnew(element){
-    
+function spawnnew(image, element){
+    const currentnum= elementcount.toString();
+    const boundrect = document.getElementById(element).getBoundingClientRect();
+    newele = document.createElement("div");
+    newele.id = currentnum
+    newele.style.position = "absolute";
+    newele.className = "draggable";
+    newele.style.backgroundImage = image;
+    console.log(boundrect.width + "px", boundrect.height + "px")
+    newele.style.minWidth = boundrect.width + "px";
+    newele.style.minHeight = boundrect.height + "px";
+    newele.style.left = boundrect.left + "px";
+    newele.style.top = boundrect.top + "px";
+    newele.onmousedown = function() {newelement(currentnum);};
+    document.body.insertBefore(newele, document.getElementById("side"));
+    subject = document.getElementById(elementcount.toString());
+    props = props.concat(document.getElementById(currentnum));
+    elementcount++;
+    delete currentnum, boundrect;
 }
-var mousedown = (evnt)=>{subject.style.left = subject.offsetLeft + "px"
-    subject.style.top = subject.offsetTop + "px"
+var mousedown = (evnt)=>{subject.style.left = subject.offsetLeft + "px";
+    subject.style.top = subject.offsetTop + "px";
 }
 var mouseup = ()=>{
     if(parseInt(subject.style.left)> window.innerWidth/5*4 && subject.id != "placehold"){
-        subject.remove()
+        subject.remove();
     }
     subject=document.getElementById("placehold");
 }
